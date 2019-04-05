0b25164cfe6c74f18b7a51a59551025b2d351e47
Michael Stegeman <michael@stegeman.me>
2019-04-05 08:22:35 -0600

    Switch packaging script from yarn to npm.

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf node_modules
-yarn install --production
+npm install --production
 rm -f SHA256SUMS
 sha256sum package.json *.js LICENSE > SHA256SUMS
 find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
